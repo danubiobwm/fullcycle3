c0d009f05596376ec5e6c69ea7a651e54e848396
José Danubio de Araujo <danubio.bwm@gmail.com>
2026-02-15 21:06:20 -0300

    migrations e testes

--- fc-monolito/src/infrastructure/api/__tests__/products.e2e.spec.ts
+++ fc-monolito/src/infrastructure/api/__tests__/products.e2e.spec.ts
@@ -1,17 +1,17 @@
-import {app, sequelize} from '../express'
+import { app, sequelize } from '../express'
 import request from 'supertest'
 
 
 describe('E2E test for product', () => {
     beforeEach(async () => {
-        await sequelize.sync({force: true});
+        await sequelize.sync({ force: true });
     });
 
     afterAll(async () => {
         await sequelize.close();
     });
 
-    it('should create a product',async () => {
+    it('should create a product', async () => {
         const response = await request(app)
             .post('/products')
             .send({
